7e60023448b998898431d284c19007318fa3b572
Gundagani <104998773+Gundagani@users.noreply.github.com>
2022-08-09 14:11:15 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,14 +10,12 @@ insert into Statement values(246,'2022-07-19',18000.57,'2022-07-30','sind12');
 insert into creditcards values(678,'SBI','MASTER','SBIN00123','creditcard','2022-07-18','pran12');
 insert into creditcards values(679,'HDFC','VISA','HDFCN00123','debitcard','2022-07-18','sind12');
 
-insert into transactions values(12345,'succesful',1300.14,'SBIN00123','done','gpay','2022-07-17','pran12');
-insert into transactions values(67890,'not succesful',1800.14,'HDFCN00123','not done','phonepay','2022-07-07','sind12');
+insert into transactions values(301,'succesful',1300.14,'SBIN00123','done','gpay','2022-07-17','pran12');
+insert into transactions values(302,'not succesful',1800.14,'HDFCN00123','not done','phonepay','2022-07-07','sind12');
 
-insert into admin values('admin');
+insert into User values(1,'Admin@123','admin');
+insert into User values(2,'Si@12345','user');
 
-insert into User values('admin','admin123','admin');
-insert into User values('pran12','12345','user');
-
-insert into Account values( 2435,'Pranathi','credit card',3450.00,'pran12');
-insert into Account values(2354,'janaki','debit card',456.00,'sind12');
+insert into Account values( 402,'Pranathi','credit card',3450.00,'pran12');
+insert into Account values(401,'janaki','debit card',456.00,'sind12');
 
